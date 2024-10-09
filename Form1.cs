d3e7c513b6fb4b3919e1ca73f22c1cd1703cf4a0
Gal Lugassi <gallugassi3@gmail.com>
2024-10-09 11:45:38 +0300

    push

--- Form1.cs
+++ Form1.cs
@@ -12,8 +12,8 @@ namespace ZombieApp
     {
         WindowsMediaPlayer player1 = new WindowsMediaPlayer();
         SoundPlayer player2;
-        SpeedyZombie character; // Replaced Character with Fighter
-        SpeedyZombie fighter; // Replaced Character with Fighter
+        SpeedyZombie character;
+        SpeedyZombie fighter;
         Zombie zombie;
         bool goLeft, goRight, goUp, goDown, gameOver;
         string facing = "right";
@@ -21,9 +21,12 @@ namespace ZombieApp
         int speed = 10;
         int Ammo = 10;
         int Score;
+        int currentLevel = 1; // משתנה למעקב אחרי השלב הנוכחי
+        int zombieHealth = 100; // כמות החיים ההתחלתית של זומבי
         Random randNum = new Random();
 
         List<PictureBox> zombiesList = new List<PictureBox>();
+        Dictionary<PictureBox, int> zombieHealthDict = new Dictionary<PictureBox, int>(); // מיפוי חיים לכל זומבי
 
         public Form1()
         {
@@ -31,22 +34,31 @@ namespace ZombieApp
             player1.URL = "Game Of Thrones.mp3";
             player2 = new SoundPlayer("sound-shot.wav");
             RestartGame();
-            character = new SpeedyZombie(); // Replaced Character with Fighter
-            fighter = new SpeedyZombie(); // Replaced Character with Fighter
+            character = new SpeedyZombie();
+            fighter = new SpeedyZombie();
 
+            healthBar.Minimum = 0;
+            healthBar.Maximum = 100;
         }
 
-        private void ResultLabel_Click(object sender, EventArgs e)
-        {
-            FormLabel formLabel = new FormLabel(Score);
-            formLabel.ShowDialog();
-            RestartGame();
-        }
         private void MainTimerEvent(object sender, EventArgs e)
         {
             CollectAmmo();
 
-            if (playerHealth > 1)
+            if (character.Health >= healthBar.Minimum && character.Health <= healthBar.Maximum)
+            {
+                healthBar.Value = character.Health;
+            }
+            else if (character.Health < healthBar.Minimum)
+            {
+                healthBar.Value = healthBar.Minimum;
+            }
+            else if (character.Health > healthBar.Maximum)
+            {
+                healthBar.Value = healthBar.Maximum;
+            }
+
+            if (character.Health > 1)
             {
                 healthBar.Value = character.Health;
             }
@@ -55,9 +67,9 @@ namespace ZombieApp
                 gameOver = true;
                 player.Image = Properties.Resources.dead;
                 GameTimer.Stop();
-                MessageBox.Show("You died.", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                string playerName = "Fighter"; // Replace with actual player name
+                ShowGameOverDialog();
             }
+
             txtAmmo.Text = "Ammo: " + Ammo;
             txtScore.Text = "Kills: " + Score;
 
@@ -94,21 +106,38 @@ namespace ZombieApp
                 }
             }
 
-            if (playerHealth < 1)
+            if (character.Health < 1)
             {
                 gameOver = true;
                 GameTimer.Stop();
-                ResultLabel_Click(null, EventArgs.Empty);
-                MessageBox.Show("You died.", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ShowGameOverDialog();
             }
 
             if (Score >= 6)
             {
-                gameOver = true;
                 GameTimer.Stop();
-                ResultLabel_Click(null, EventArgs.Empty);
-                MessageBox.Show("You killed the zombies. You win!", "Congratulations", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                currentLevel++; // העלאת השלב
 
+                if (currentLevel > 5) // אם הגענו לשלב 6, סיימנו את המשחק
+                {
+                    MessageBox.Show("You have defeated all the zombies and completed the game!", "Game Completed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    RestartGame();
+                }
+                else
+                {
+                    MessageBox.Show($"Congratulations! You completed level {currentLevel - 1}. Moving to level {currentLevel}!", "Level Up", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    RestartGame();
+                }
+            }
+        }
+
+        private void ShowGameOverDialog()
+        {
+            DialogResult result = MessageBox.Show("You died. Do you want to restart the game from level 1?", "Game Over", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                currentLevel = 1; // כאשר מתים, חוזרים תמיד לשלב 1
+                RestartGame();
             }
         }
 
@@ -236,11 +265,17 @@ namespace ZombieApp
                 {
                     if (bullet.Bounds.IntersectsWith(zombie.Bounds))
                     {
-                        Controls.Remove(zombie);
-                        zombiesList.Remove(zombie);
+                        zombieHealthDict[zombie] -= 20;
                         Controls.Remove(bullet);
-                        Score++;
-                        break;
+                        bulletTimer.Stop();
+
+                        if (zombieHealthDict[zombie] <= 0)
+                        {
+                            Controls.Remove(zombie);
+                            zombiesList.Remove(zombie);
+                            Score++;
+                            break;
+                        }
                     }
                 }
             };
@@ -255,23 +290,17 @@ namespace ZombieApp
 
         private void MakeZombies()
         {
-            Zombie zombie = new Zombie();
             PictureBox zombiePictureBox = new PictureBox();
-            zombie.Health = 100;
-            zombie.Speed = 6;
-            Controls.Add(zombiePictureBox);
-            zombiePictureBox.BringToFront();
             zombiePictureBox.Tag = "zombie";
             zombiePictureBox.Image = Properties.Resources.zleft;
             zombiePictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
             zombiePictureBox.Left = ClientSize.Width + randNum.Next(100, 700);
             zombiePictureBox.Top = randNum.Next(20, ClientSize.Height - zombiePictureBox.Height);
             zombiesList.Add(zombiePictureBox);
+            zombieHealthDict[zombiePictureBox] = zombieHealth;
             Controls.Add(zombiePictureBox);
             zombiePictureBox.BringToFront();
         }
-
-        
 
         private void DropAmmo()
         {
@@ -304,15 +333,17 @@ namespace ZombieApp
 
         private void RestartGame()
         {
-            zombie = new Zombie();
+            character = new SpeedyZombie();
+            zombieHealth = currentLevel * 50;
             player.Image = Properties.Resources.right;
             foreach (PictureBox zombie in zombiesList)
             {
                 Controls.Remove(zombie);
             }
             zombiesList.Clear();
+            zombieHealthDict.Clear();
 
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < currentLevel * 6; i++)
             {
                 MakeZombies();
             }
@@ -324,10 +355,13 @@ namespace ZombieApp
             gameOver = false;
 
             playerHealth = 100;
+            character.Health = 100;
             speed = 10;
             Ammo = 10;
-            zombie.Speed = 6;
             Score = 0;
+
+            player.Left = 100;
+            player.Top = 100;
 
             healthBar.Value = playerHealth;
             txtAmmo.Text = "Ammo: " + Ammo;
@@ -335,11 +369,5 @@ namespace ZombieApp
 
             GameTimer.Start();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            player1.controls.play();
-        }
-        
     }
 }
